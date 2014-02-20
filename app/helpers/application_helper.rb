983b486bd9cba6ceb37f74aed64ba24a99329508
m.yamaoka <arcs7nine@gmail.com>
2014-02-20 22:44:24 +0900

    シンタックスエラーを修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   
-  DIFFICULTIES_TEXT_COLOR = {'EASY' => 'text-info', 'NORMAL' => 'text-success', 'HARD' => 'text-warning', 'EXTREME' => 'text-danger'}
+  DIFFICULTIES_TEXT_COLOR = {'EASY' => 'text-info', 'NORMAL' => 'text-success', 'HARD' => 'text-warning', 'EXTREME' => 'text-error'}
 
   def get_difficulty_color(difficulty)
 
