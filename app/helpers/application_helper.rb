d458716f46cd9a5f9b6b64a364a614d423ab683d
m.yamaoka <arcs7nine@gmail.com>
2014-02-20 16:44:55 +0900

    難易度別にテキストを色分けする

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  
+  DIFFICULTIES_TEXT_COLOR = {'EASY' => 'text-info', 'NORMAL' => 'text-success', 'HARD' => 'text-warning', 'EXTREME' => 'text-danger'}
+
+  def get_difficulty_color(difficulty)
+
+    DIFFICULTIES_TEXT_COLOR[difficulty]
+
+  end
+
 end
