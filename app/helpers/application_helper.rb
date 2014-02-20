8ea5bc3b87b15642ede51e83b25e657e8a75b728
m.yamaoka <arcs7nine@gmail.com>
2014-02-21 03:43:49 +0900

    デザイン調整

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,25 @@
 module ApplicationHelper
   
-  DIFFICULTIES_TEXT_COLOR = {'EASY' => 'text-info', 'NORMAL' => 'text-success', 'HARD' => 'text-warning', 'EXTREME' => 'text-error'}
+  DIFFICULTY_CLASS = {
+    text:  {EASY: 'text-info',  NORMAL: 'text-success',  HARD: 'text-warning',  EXTREME: 'text-error'},
+    label: {EASY: 'label-info', NORMAL: 'label-success', HARD: 'label-warning', EXTREME: 'label-danger'},
+    table: {EASY: 'info',       NORMAL: 'success',       HARD: 'warning',       EXTREME: 'error'},
+  }
 
-  def get_difficulty_color(difficulty)
+  def get_difficulty_text(difficulty)
+    get_difficulty_class(:text, difficulty)
+  end
 
-    DIFFICULTIES_TEXT_COLOR[difficulty]
+  def get_difficulty_label(difficulty)
+    get_difficulty_class(:label, difficulty)
+  end
 
+  def get_difficulty_table(difficulty)
+    get_difficulty_class(:table, difficulty)
+  end
+
+  private def get_difficulty_class(class_key, difficulty)
+    DIFFICULTY_CLASS[class_key][difficulty.intern]
   end
 
 end
