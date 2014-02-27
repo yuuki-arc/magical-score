988e4f2fc45e4c54f1c1d3b2ec93ad47165c3944
m.yamaoka <arcs7nine@gmail.com>
2014-02-27 23:29:30 +0900

    heroku用設定を追加

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -97,4 +97,7 @@ DivaAcScore::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  # For heroku
+  config.serve_static_assets = true
 end
