784ca1d257cca488dd8971346d99439f35a9985c
m.yamaoka <arcs7nine@gmail.com>
2014-03-05 03:15:47 +0900

    アプリケーション名を変更

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-DivaAcScore::Application.config.session_store :cookie_store, key: '_diva-ac-score_session'
+DivaAcScore::Application.config.session_store :cookie_store, key: '_diva-portable_session'
