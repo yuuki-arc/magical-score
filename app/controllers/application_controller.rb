41343a2b5db8ffcde8134246df3939fd77970d02
m.yamaoka <arcs7nine@gmail.com>
2014-03-06 14:19:01 +0900

    BASIC認証をかけ直した

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,5 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   layout 'slate'
 
-#  http_basic_authenticate_with :name => ENV["BASIC_AUTH_NAME"], :password => ENV["BASIC_AUTH_PW"] if Rails.env.production?
+  http_basic_authenticate_with :name => ENV["BASIC_AUTH_NAME"], :password => ENV["BASIC_AUTH_PW"] if Rails.env.production?
 end
