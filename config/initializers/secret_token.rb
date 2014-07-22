2e39de05f1c57634d5ae11bf823b033aa24c1586
yuuki-arc <arcs7nine@gmail.com>
2014-07-22 13:58:17 +0900

    secret tokenを環境変数から取得するように変更

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-DivaAcScore::Application.config.secret_key_base = '0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef'
+DivaAcScore::Application.config.secret_key_base = ENV['SECRET_TOKEN']
