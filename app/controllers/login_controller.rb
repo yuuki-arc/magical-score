b7ead443b492ad20bc80cfd95433e74c9bfd0624
yuuki-arc <arcs7nine@gmail.com>
2014-07-24 14:00:33 +0900

    ログインindex追加

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -1,6 +1,10 @@
 class LoginController < ApplicationController
   skip_before_filter :check_logined
 
+  def index
+      render 'index'
+  end
+  
   def auth
     user = User.authenticate(params[:name], params[:password])
     if user then
