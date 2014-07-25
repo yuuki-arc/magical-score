6979e5fcab031f0c39d47fe4f9db9758f3c1932d
yuuki-arc <arcs7nine@gmail.com>
2014-07-25 14:11:22 +0900

    ユーザー情報をDBに保持して認証にトークンを使うように修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,19 @@
-class User
-  attr_accessor :access_code, :password
+class User < ActiveRecord::Base
+  attr_accessible :access_code, :password
+  has_secure_password
+  before_create { generate_token(:auth_token) }
 
   def initialize(access_code, password)
     @access_code = access_code
     @password = password
   end
 
+  def generate_token(column)
+    begin
+      self[column] = SecureRandom.urlsafe_base64
+    end while User.exists?(column => self[column])
+  end
+  
   def self.authenticate(access_code, password)
     self.new(access_code, password)
   end
