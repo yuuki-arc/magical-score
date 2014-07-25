e6e22d1ca0d0a09491219eba7540b715a7727be0
yuuki-arc <arcs7nine@gmail.com>
2014-07-25 13:41:05 +0900

    ログイン情報を保持する仕組みを追加

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -2,10 +2,14 @@ class LoginController < ApplicationController
   skip_before_filter :check_logined
 
   def index
-      render 'index'
+    @access_code = cookies[:access_code]
+    @password = cookies[:password]
+    render 'index'
   end
   
   def auth
+    cookies[:access_code] = params[:access_code]
+    cookies[:password] = params[:password]
     user = User.authenticate(params[:access_code], params[:password])
     if user then
       session[:user] = user
@@ -13,6 +17,8 @@ class LoginController < ApplicationController
       redirect_to :controller => 'music_lists', :action => 'index'
     else
       flash.now[:referer] = params[:referer]
+      @access_code = cookies[:access_code]
+      @password = cookies[:password]
       @error = 'アクセスコードまたはパスワードが一致しません。'
       render 'index'
     end
