c91f90f5c79d9feb17d5dabe0e416842a825e3c8
yuuki-arc <arcs7nine@gmail.com>
2014-07-26 10:45:33 +0900

    認証回りの機能実装

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140725045846) do
+ActiveRecord::Schema.define(version: 20140726012121) do
 
   create_table "users", force: true do |t|
     t.string   "access_code"
-    t.string   "password"
+    t.string   "password_digest"
     t.string   "auth_token"
     t.datetime "created_at"
     t.datetime "updated_at"
